521e7398fdae44c5ad1cd5a4ded727c162c5c535
nemethzsofii <161205354+nemethzsofii@users.noreply.github.com>
2025-03-26 20:18:00 +0100

    bugfix utils.js

--- WebApplication1/Controllers/UserController.cs
+++ WebApplication1/Controllers/UserController.cs
@@ -22,7 +22,7 @@ public class UserController : ControllerBase
         _context = context;
     }
     // 🔹 GET: api/User/CurrentUser (Fetch current user)
-    [HttpGet]
+    [HttpGet("Current")]
     public async Task<ActionResult<User>> GetCurrentUser()
     {
         int currentUserId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
