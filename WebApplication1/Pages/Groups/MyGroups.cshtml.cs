051bb5102db9615148a15ba45f11658df7be38ba
nemethzsofii <161205354+nemethzsofii@users.noreply.github.com>
2025-03-26 19:39:54 +0100

    change func name

---
+++ WebApplication1/Pages/Groups/MyGroups.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Pages.Groups
+{
+    public class MyGroupsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
