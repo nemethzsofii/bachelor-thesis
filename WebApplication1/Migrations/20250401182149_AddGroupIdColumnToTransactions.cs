b562767796824d3942836c94281c64a727108329
nemethzsofii <161205354+nemethzsofii@users.noreply.github.com>
2025-04-02 06:23:56 +0200

    migration - addGroupIdToTransactions

---
+++ WebApplication1/Migrations/20250401182149_AddGroupIdColumnToTransactions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGroupIdColumnToTransactions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "GroupId",
+                table: "Transactions",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GroupId",
+                table: "Transactions");
+        }
+    }
+}
