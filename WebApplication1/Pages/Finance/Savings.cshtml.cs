712039f7c9a5ca73ef7db9e070dee163b6fc9f58
nemethzsofii <161205354+nemethzsofii@users.noreply.github.com>
2025-04-13 09:32:34 +0200

    create savings page

---
+++ WebApplication1/Pages/Finance/Savings.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Pages.Finance
+{
+    public class SavingsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
